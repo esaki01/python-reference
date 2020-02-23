dfe5676330c0f92d1dff5b8c275639d71cd4c3c6
esaki01 <social0430@gmail.com>
2020-02-23 18:39:48 +0900

    Add functools samples

---
+++ scripts/functools/wraps/lesson.py
@@ -0,0 +1,21 @@
+import functools
+
+
+def d(f):
+    @functools.wraps(f)
+    def w():
+        """Wrapper docstring"""
+        print('decorator')
+        return f()
+
+    return w
+
+
+@d
+def example():
+    """Example docstring"""
+    print('example')
+
+
+print(example.__doc__)
+help(example)
